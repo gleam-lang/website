3937c0aabf756f9155c38b48a3eedc6b8af0fdca
Louis Pilfold <louis@lpil.uk>
2021-06-04 22:34:20 +0100

    v0.16.0 demo widgets

---
+++ _posts/v0.16.0/out/dogs.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const url = "https://dog.ceo/api/breeds/image/random";
+
+export function main() {
+  let button = query_selector("[data-dogs-button]");
+  let img = query_selector("[data-dogs-img]");
+  new_dog(img);
+  return set(button, "onclick", () => { return new_dog(img); });
+}
+
+function new_dog(img) {
+  return then(
+    then(http_fetch(url), get_json_body),
+    (json) => {
+      set(img, "src", get(json, "message"));
+      return resolve_promise(undefined);
+    },
+  );
+}
+
+function query_selector(arg0) {
+  return document.querySelector(arg0)
+}
+
+function get(from, property) {
+  return Reflect.get(from, property)
+}
+
+function set(on, property, to) {
+  return Reflect.set(on, property, to)
+}
+
+function http_fetch(arg0) {
+  return fetch(arg0)
+}
+
+function get_json_body(arg0) {
+  return Response.prototype.json.call(arg0)
+}
+
+function then(arg0, arg1) {
+  return Promise.prototype.then.call(arg0, arg1)
+}
+
+function resolve_promise(arg0) {
+  return Promise.resolve(arg0)
+}
