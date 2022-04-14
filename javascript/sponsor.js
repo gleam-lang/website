916b27e1f3c4cb726f7944177d320d709cb5fc52
Herminio Torres <herminiocesar@gmail.com>
2022-04-14 19:50:19 -0300

    change double quote for single quote

--- javascript/sponsor.js
+++ javascript/sponsor.js
@@ -1,4 +1,4 @@
-const sponsorLink = document.getElementsByClassName("sponsor-nav-link")[0]
+const sponsorLink = document.getElementsByClassName('sponsor-nav-link')[0]
 
 sponsorLink.addEventListener('mouseover', scaleDecreaseHeart)
 
@@ -6,10 +6,10 @@ sponsorLink.addEventListener('mouseout',  scaleIncreaseHeart)
 
 function scaleIncreaseHeart() {
   let svg = sponsorLink.getElementsByTagName('svg')[0];
-  svg.setAttribute("transform", `scale(1)`);
+  svg.setAttribute('transform', `scale(1)`);
 }
 
 function scaleDecreaseHeart(){
   let svg = sponsorLink.getElementsByTagName('svg')[0];
-  svg.setAttribute("transform", `scale(1.2)`);
+  svg.setAttribute('transform', `scale(1.2)`);
 }
