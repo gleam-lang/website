587f4174d1880ec2eba935b95d241c9751d04bc2
Herminio Torres <herminiocesar@gmail.com>
2022-04-14 19:46:13 -0300

    fix sponsor button

---
+++ javascript/sponsor.js
@@ -0,0 +1,15 @@
+const sponsorLink = document.getElementsByClassName("sponsor-nav-link")[0]
+
+sponsorLink.addEventListener('mouseover', scaleDecreaseHeart)
+
+sponsorLink.addEventListener('mouseout',  scaleIncreaseHeart)
+
+function scaleIncreaseHeart() {
+  let svg = sponsorLink.getElementsByTagName('svg')[0];
+  svg.setAttribute("transform", `scale(1)`);
+}
+
+function scaleDecreaseHeart(){
+  let svg = sponsorLink.getElementsByTagName('svg')[0];
+  svg.setAttribute("transform", `scale(1.2)`);
+}
