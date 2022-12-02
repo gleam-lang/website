9b07b56fac62876637e39d2487c6f4ad6adc9f85
Louis Pilfold <louis@lpil.uk>
2022-12-02 18:47:58 +0000

    Sponsors expanding

--- new.js
+++ new.js
@@ -2,6 +2,10 @@ function showPride() {
   document.body.classList.toggle("pride");
 }
 
+function expandSponsorsSection() {
+  document.documentElement.style.setProperty("--sponsors-max-height", "5000px");
+}
+
 if (new Date().getMonth() === 6) showPride();
 
 for (let element of document.querySelectorAll("[data-show-pride]")) {
@@ -12,4 +16,8 @@ for (let element of document.querySelectorAll("[data-randomise-order]")) {
   let children = Array.from(element.children);
   children.sort(() => Math.random() - 0.5);
   element.replaceChildren(...children);
+}
+
+for (let element of document.querySelectorAll("[data-expand-sponsors]")) {
+  element.addEventListener("click", expandSponsorsSection);
 }
